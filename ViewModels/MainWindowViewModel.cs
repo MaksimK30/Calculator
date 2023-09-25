3127ab45179ce5d1e5f63a0472f9a5f68b496ead
MaksimK30 <makskuzmih@mail.ru>
2023-09-25 09:26:47 +0300

    Настроена конвертация в польскую нотацию

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Input;
 using ReactiveUI;
+using Expression = System.Linq.Expressions.Expression;
 
 namespace Calculator.ViewModels;
 
@@ -12,31 +13,41 @@ public class MainWindowViewModel : ViewModelBase
     private LinkedList<double> MemoryList = new LinkedList<double>();
     private List<string> SqrtList = new List<string>();
     private int MemoryPointer;
+    private Dictionary<char, int> OperationsPriopity = new Dictionary<char, int>()
+    {
+        {'-', 0}, {'+', 0},
+        {'*', 1}, {'/', 1},
+        {'√', 2}
+    };
 
     public MainWindowViewModel()
     {
-        Result = "";
+        Result = "√16+8*2-4+√25/7";
+        Result = "1/5-4*√9/15-32";
         MemoryPointer = 0;
     }
     public ICommand PlusCmd()
     {
-        Result += "+";
+        if (Result.Length != 0 && ! ".-+/*√".Contains(Result[Result.Length - 1]))
+            Result += "+";
         return null;
     }
     public ICommand MinusCmd()
     {
-        if (Result.Length == 0 || ! ".-+/*√".Contains(Result[Result.Length - 1]))
+        if (Result.Length != 0 && ! ".-+/*√".Contains(Result[Result.Length - 1]))
             Result += "-";
         return null;
     }
     public ICommand MultiplyCmd()
     {
-        Result += "*";
+        if (Result.Length != 0 && ! ".-+*/√".Contains(Result[Result.Length - 1]))
+            Result += "*";
         return null;
     }
     public ICommand SplitCmd()
     {
-        Result += "/";
+        if (Result.Length != 0 && ! ".-+*/√".Contains(Result[Result.Length - 1]))
+            Result += "/";
         return null;
     }
     public ICommand NumInput(object Parameter)
@@ -77,7 +88,7 @@ public class MainWindowViewModel : ViewModelBase
     public ICommand MemorySaveCmd()
     {
         if (!Result.Contains("*") && !Result.Contains("/") && !Result.Contains("+") && !Result.Contains("-")
-            && !Result.Contains("√") && Result.Length != 0)
+            && !Result.Contains("√") && Result.Length != 0 && Result != "Error")
         {
             MemoryList.AddFirst(Convert.ToDouble(Result));
             if (MemoryList.Count > 3)
@@ -90,7 +101,70 @@ public class MainWindowViewModel : ViewModelBase
     }
     public ICommand CalculateCmd()
     {
-        
+        if (Result.Length == 0)
+        {
+            Result = "0";
+        }
+
+        if ("/*-+.√".Contains(Result[Result.Length-1]))
+        {
+            Result = "Error";
+        }
+
+        int NumIndex = 0;
+        List<string> OutputStr = new List<string>();
+        List<char> Stack = new List<char>();
+        string TmpDigit = "";
+        for (int i = 0; i < Result.Length; i++)
+        {
+            if (Char.IsDigit(Result[i]) || Result[i] == '.')
+            {
+                TmpDigit += Result[i];
+            }
+            else
+            {
+                if (TmpDigit != "")
+                {
+                    OutputStr.Add(TmpDigit);
+                    TmpDigit = "";
+                }
+
+                if (Stack.Count == 0 || 
+                OperationsPriopity[Stack.Last()] < OperationsPriopity[Result[i]])
+                {
+                    Stack.Add(Result[i]);
+                }
+                else
+                {
+                    bool Flag = true;
+                    while (Flag)
+                    {
+                        if (Stack.Count != 0 && OperationsPriopity[Stack.Last()] >= OperationsPriopity[Result[i]])
+                        {
+                            OutputStr.Add(Stack.Last().ToString());
+                            Stack.Remove(Stack.Last());
+                        }
+                        else
+                        {
+                            Stack.Add(Result[i]);
+                            Flag = false;
+                        }
+                    }
+                }
+            }
+
+            if (i == Result.Length - 1)
+            {
+                OutputStr.Add(TmpDigit);
+                for (int j = 0; j < Stack.Count;)
+                {
+                    OutputStr.Add(Stack.Last().ToString());
+                    Stack.Remove(Stack.Last());
+                }
+            }
+        }
+
+        Result = String.Join("", OutputStr.ToArray());
         return null;
     }
     public ICommand ClearMemoryCmd()
